a38b1d8c8d43492921ff7830a946972a0d4e7c40
lois <loisjanequillo@gmail.com>
2020-11-08 00:23:20 +0800

    Fixed layout for stories page

--- lib/pages/stories.dart
+++ lib/pages/stories.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'common_widgets/page_title.dart';
+import './common_widgets/dict_button.dart';
 
 class Stories extends StatefulWidget {
   @override
@@ -7,27 +9,51 @@ class Stories extends StatefulWidget {
 }
 
 class _StoriesState extends State<Stories> {
+  final List<List<String>> storyList = [
+    ['Story1', 'u'],
+    ['Story2', 'l'],
+    ['Story3', 'l'],
+    ['Story4', 'l'],
+    ['Story5', 'l']
+  ];
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('App Name'),
-      ),
-      body: new Container(
-          child: new Column(
-        children: [
-          Text(
-            'Stories',
-            style: GoogleFonts.montserrat(
-              textStyle: TextStyle(
-                color: Colors.black,
-                fontSize: 30.0,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-        ],
-      )),
+    return new PageTitle(
+        pageTitle: 'Stories',
+        pageGreeting: 'Let\'s Read!',
+        pageChild: _pageContent(context));
+  }
+
+  Widget _pageContent(context) {
+    return Column(
+      children: [
+        Container(
+            height: MediaQuery.of(context).size.height - 200,
+            child: GridView.count(
+              crossAxisCount: 1,
+              mainAxisSpacing: 0,
+              crossAxisSpacing: 0,
+              childAspectRatio: 4.5,
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(20),
+              children: [
+                ...storyList.map(
+                  (i) => Container(
+                    color: Colors.transparent,
+                    child: DictButton(
+                      buttonName: i.first,
+                      buttonImage: i.last == 'u'
+                          ? 'images/unlock.png'
+                          : 'images/lock.png',
+                      onPressed: () {},
+                      imgH: 40,
+                      imgW: 40,
+                    ),
+                  ),
+                )
+              ],
+            ))
+      ],
     );
   }
 }
